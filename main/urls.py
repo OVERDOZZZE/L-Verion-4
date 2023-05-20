718179fdf607039771993cb2630acfa868667a92
OVERDOZZZE <saparbekov113@gmail.com>
2023-05-21 02:47:43 +0600

    commit

--- main/urls.py
+++ main/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("all_books/", books, name='all_books'),
+    path("", books, name='all_books'),
     path("book_detail/<int:id>/", book_detail, name='book_detail'),
-    path("add_book/", add_book),
+    path("add_book/", add_book, name='addb'),
     path("book_detail/<int:id>/update_book/", update_book, name='update_book'),
-    path("add_author/", add_author),
-    path("add_publisher/", add_publisher),
+    path("add_author/", add_author, name='adda'),
+    path("add_publisher/", add_publisher, name='addp'),
     path('login/', login_view, name='login')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
